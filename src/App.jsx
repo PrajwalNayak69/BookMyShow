d84a2257e9292398ce3dcb0a935c7cfa210d99a9
Prajwal Nayak <prajwal@Prajwals-MacBook-Air.local>
2021-07-28 15:09:22 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div className="App p-10">
+      <button className="border-2 px-2 py-2 
+      border-blue-500 
+      rounded bg-blue-500
+       text-white 
+       hover:bg-transparent
+        hover:border-2 
+        hover:text-blue-500
+         focus:border-red-300">
+        hello
+      </button>
+    </div>
+  );
+}
+
+export default App;
